c8b15aba79b26944bdfd4c41cde241f104949146
Toast1e <64542429+Toast1e@users.noreply.github.com>
2023-05-01 23:31:50 +0200

    First and Final

---
+++ Dupe.py
@@ -0,0 +1,15 @@
+import json
+
+with open('engData.json') as f:
+    data = json.load(f)
+
+unique_values = {}
+for item in data:
+    for value in item.values():
+        if value in unique_values:
+            unique_values[value] += 1
+        else:
+            unique_values[value] = 1
+
+duplicates = [value for value, count in unique_values.items() if count > 1]
+print(duplicates)
